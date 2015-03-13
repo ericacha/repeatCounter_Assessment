6688a50e6d00fd712d89d0d394994d9fcbfbba9d
Erica <chae_osu@yahoo.com>
2015-03-13 14:46:13 -0700

    SPEC #2 will not run.

--- src/repeatCountGenerator.php
+++ src/repeatCountGenerator.php
@@ -3,14 +3,27 @@
     class RepeatCounter
     {
 
-        function countRepeats($input)
+        function countRepeats($inputLetters,$inputWords)
         {
-        $input = $letter;
-        $letter = "A";
-        if ($result = substr_count($letter,"A")) {
-            return $result;
+        $inputLetters = strtolower($inputLetters);
+        $inputWords = strtolower($inputWords);
+
+
+        if (substr_count($inputLetters == $inputWords)) {
+              return '';
         }
-        }
+
+
+
+      }
+
+
+        // $input = $letter;
+        // $letter = "A";
+        // if ($result = substr_count($letter, "A")) {
+        //     return $result;
+        // }
+
 
 
 
--- tests/stringRepeatTest.php
+++ tests/stringRepeatTest.php
@@ -10,16 +10,32 @@
 
           //Arrange
           $test_RepeatCounter = new RepeatCounter;
-          $input = "A";
+          $inputLetters = "A";
+          $inputWords = "A";
+
 
           //Act
-          $result = $test_RepeatCounter->countRepeats($input);
+          $result = $test_RepeatCounter->countRepeats($inputWords, $inputLetters);
 
           //Assert
-          $this->assertEquals(1, $result);
+          $this->assertEquals(2, $result);
 
         }
 
+        // function test_countRepeats_AA()
+        // {
+        //
+        //   //Arrange
+        //   $test_RepeatCounter_AA = new RepeatCounter;
+        //   $input = "A,A";
+        //
+        //   //Act
+        //   $result = $test_RepeatCounter_AA->countRepeats($input);
+        //
+        //   //Assert
+        //   $this->assertEquals(2, $result);
+        //
+        // }
 
     }
 
